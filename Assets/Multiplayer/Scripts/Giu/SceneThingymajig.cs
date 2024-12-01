ef888a61baa4b8a52c4b26a3c42d773d68e2bdbc
ChrisFuchs12 <157928174+ChrisFuchs12@users.noreply.github.com>
2024-12-01 20:56:08 +0000

    Revert "adwa"
    This reverts commit 0bc142232e6b3bd6a024451f1b9374096b63bd63.

--- Assets/Multiplayer/Scripts/Giu/SceneThingymajig.cs
+++ Assets/Multiplayer/Scripts/Giu/SceneThingymajig.cs
@@ -7,12 +7,9 @@ public class SceneThingymajig : MonoBehaviour
 {
 
     public GameObject loadingScrn;
-    public GameObject icon;
-
 
     public void PlayGame(){
         loadingScrn.SetActive(true);
-        icon.SetActive(false);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
