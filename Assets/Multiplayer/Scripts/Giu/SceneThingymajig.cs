02d9e771b013e151c08d982ca218075d365a2349
ChrisFuchs12 <157928174+ChrisFuchs12@users.noreply.github.com>
2024-12-01 21:16:51 +0000

    aaaaa

--- Assets/Multiplayer/Scripts/Giu/SceneThingymajig.cs
+++ Assets/Multiplayer/Scripts/Giu/SceneThingymajig.cs
@@ -7,9 +7,13 @@ public class SceneThingymajig : MonoBehaviour
 {
 
     public GameObject loadingScrn;
+    public GameObject icon;
+    public GameObject playButton;
 
     public void PlayGame(){
         loadingScrn.SetActive(true);
+        icon.SetActive(false);
+        playButton.SetActive(false);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
